8e5a9118a549623113b81f6a24326319ca057454
Ahmet E. Dag <adag21044@gmail.com>
2024-08-27 21:28:11 +0300

    progress bar doesn't work

--- Assets/Scripts/CoinDisplay.cs
+++ Assets/Scripts/CoinDisplay.cs
@@ -3,53 +3,8 @@ using TMPro;
 
 public class CoinDisplay : Observer
 {
-    public float maxInc = 5f;
-    public TextMeshProUGUI coinText; 
-    [SerializeField]private int currentCoinCount;
-    private ProgressBar progressBar;
-
-    private void Start()
-    {
-        var notifier = FindObjectOfType<Notifier>();
-        if (notifier != null)
-        {
-            notifier.AddObserver(this);
-        }
-
-        progressBar = FindObjectOfType<ProgressBar>();
-        if (progressBar == null)
-        {
-            Debug.LogError("ProgressBar component not found!");
-        }
-    }
-
     public override void OnNotify(NotificationTypes notificationType)
     {
-        float incAmount = 1f;
-        maxInc = maxInc - 1f;
-        if (notificationType == NotificationTypes.CoinIncrease)
-        {
-            currentCoinCount++;
-            coinText.text = "Coins: " + currentCoinCount;
-
-            if (progressBar != null && maxInc > 0f)
-            {
-                progressBar.StartIncreasingWidth(incAmount);
-            }
-            else if(maxInc <= 0f) return;
-            else
-            {
-                Debug.LogError("ProgressBar reference is null!");
-            }
-        }
-    }
-
-    private void OnDestroy()
-    {
-        var notifier = FindObjectOfType<Notifier>();
-        if (notifier != null)
-        {
-            notifier.RemoveObserver(this);
-        }
+        throw new System.NotImplementedException();
     }
 }
